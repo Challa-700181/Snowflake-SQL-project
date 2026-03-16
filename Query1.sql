7d676fa6a84ef3cb3f4b66ac7ab2a1c4493b4bb7
Challa-700181 <challasaisindhu7@gmail.com>
2026-03-15 20:35:39 -0400

    Delete Query1.sql

--- Query1.sql
+++
@@ -1,4 +0,0 @@
-SELECT ENTITY AS Country_name,MAX("Current number of COVID-19 patients in ICU per million") AS Max_icu_per_million
-FROM GLOBAL_DATASET.PUBLIC.COVID_DATA
-GROUP BY Country_name
-ORDER BY Max_icu_per_million DESC;
